ecf706d18ed5eed832e2ffaf1b981412ad9dda92
raugh135 <55518299+raugh135@users.noreply.github.com>
2020-01-06 12:00:35 +0800

    2020-1-6

---
+++ Assets/GM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+
+	public void ThrowPickUp()
+    {
+
+    }
+
+    public void ThrowDetach(GameObject obj)
+    {
+        Destroy(obj);
+    }
+
+    public void ThrowHeld()
+    {
+
+    }
+}
